46143f2a488e3372df3f89dc4ba140d6ff6d946f
Igor Pupkin <igorpupkinable@users.noreply.github.com>
2026-09-23 10:55:37 +0300

    Convert to NodeJS ESM

--- fio-report-parser.mjs
+++ fio-report-parser.mjs
@@ -1,5 +1,5 @@
-const { resolve } = require('node:path');
-const { cwd } = require('node:process');
+import path from 'node:path';
+import { cwd } from 'node:process';
 
 /*
 SSD tests. Not supported yet.
@@ -100,7 +100,7 @@ const ns2ms = (ns) => ns / 1000000;
 
 if (process.argv.length === 2) {
   console.error('\x1b[31m%s\x1b[0m', 'Please provide FIO test results in JSON format.');
-  console.log('\x1b[33mExample:\x1b[0m %s', `node ${__filename} ./path/to/report.json`);
+  console.log('\x1b[33mExample:\x1b[0m %s', `node ${path.basename(import.meta.url)} ./path/to/report.json`);
 
   process.exit(1);
 }
@@ -115,7 +115,10 @@ const parsingMessage = `Parsing ${filepath}`;
 
 console.time(parsingMessage);
 
-const report = require(resolve(cwd(), filepath));
+const { default: report } = await import(
+  path.resolve(cwd(), filepath),
+  { with: { type: 'json' } },
+);
 
 console.timeEnd(parsingMessage);
 
